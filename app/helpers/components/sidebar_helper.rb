da1aece76c254eb0be3e19c3eda9648203c7c119
Yonghyun Kim (Fred) <yhkks1038@gmail.com>
2020-06-14 16:57:59 +0900

    사이드바 활성상태 연산 방법 수정

--- app/helpers/components/sidebar_helper.rb
+++ app/helpers/components/sidebar_helper.rb
@@ -1,9 +1,14 @@
 module Components
   module SidebarHelper
     def sidebar_list_item(text, path, option = {}, &block)
-      _path = URI.encode(URI.decode_www_form_component(path).strip)
-      option[:active] ||= request.path.include?(_path) && request.path.first(5) == _path.first(5)
+      option[:active] ||= match_path(path, request.url) # request.path.include?(_path) && request.path.first(5) == _path.first(5)
       render 'layouts/components/sidebar/list-item', text: text, path: path, option: option, &block
+    end
+
+    def match_path(base, compare)
+      base_uri = URI.parse(URI.encode(URI.decode_www_form_component(base).strip))
+      compare_uri = URI.parse(compare)
+      base_uri.path.present? && compare_uri.path.start_with?(base_uri.path)
     end
   end
 end
